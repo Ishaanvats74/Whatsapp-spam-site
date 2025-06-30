8df7acfc9dbcb8e74cd9740655636fed88465a15
Ishaan vats <Ishaanvats74@gmail.com>
2025-06-30 21:07:08 +0530

    added python loop and ui changes

--- spam.py
+++ spam.py
@@ -1,6 +1,14 @@
 import pywhatkit
 import pyautogui
+import time
 
+mobile_number = int(input())
+message = input()
+loop = int(input())
 
-pywhatkit.sendwhatmsg_instantly("+917011160313", "Hi",wait_time=6)
+pywhatkit.sendwhatmsg_instantly(f"+91{mobile_number}", message ,wait_time=6)
 pyautogui.press("enter")
+for i in range(loop-1):
+    pyautogui.write(message)
+    pyautogui.press("enter")
+    time.sleep(0.5)
