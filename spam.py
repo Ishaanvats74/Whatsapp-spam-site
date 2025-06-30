f3deaccc7cfd7e5fb6d9c15a7c6708597ad85352
Ishaan vats <Ishaanvats74@gmail.com>
2025-06-30 22:15:20 +0530

    added backend logic but when running flask it is not running

--- spam.py
+++ spam.py
@@ -1,14 +1,28 @@
 import pywhatkit
 import pyautogui
 import time
+from flask import Flask, render_template, request
 
-mobile_number = int(input())
-message = input()
-loop = int(input())
+app = Flask(__name__)
 
-pywhatkit.sendwhatmsg_instantly(f"+91{mobile_number}", message ,wait_time=6)
-pyautogui.press("enter")
-for i in range(loop-1):
-    pyautogui.write(message)
+@app.route('/',methods=['GET'])
+def home():
+    print("Flask server is running")
+    return render_template('spamSite.html')
+
+@app.route('/spamSite',methods=['POST'])
+def spam():
+    mobile_number = int(request.form.get('MobileNumber'))
+    message = request.form.get('Message')
+    Count = int(request.form.get('RepeatCount'))
+
+    pywhatkit.sendwhatmsg_instantly(f"+91{mobile_number}", message ,wait_time=10)
     pyautogui.press("enter")
-    time.sleep(0.5)
+    for i in range(Count-1):
+        pyautogui.write(message)
+        pyautogui.press("enter")
+        time.sleep(0.5)
+    return render_template('spamSite.html', result="Spam messages sent successfully!")
+if __name__ == '__main__':
+    app.run(debug=True)
+    
